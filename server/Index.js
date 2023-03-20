5f937cf4ac2d9461ac0c8ca7c7acca16b537e8f0
sukantad <ghoshsu4@gmail.com>
2023-03-20 14:05:01 +0530

    ai file created

---
+++ server/Index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import dotenv from "dotenv";
+import { Configuration, OpenAIApi } from "openai";
+
+const app = express();
+dotenv.config();
+app.use(express.json());
+
+const configuration = new Configuration({
+  apiKey: process.env.KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+app.get("/", (req, res) => {
+  try {
+    res.send("Welcome");
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.post("/ai", async (req, res) => {
+  try {
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: "How are you?",
+    });
+    res.status(200).send(response.data.choices[0].text);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+app.listen(3050, () => {
+  console.log("Server is Running");
+});
