bc2d75a421bbe4ccbd377f10a1ff9d8506ebc5b2
sukantad <ghoshsu4@gmail.com>
2023-03-29 18:32:48 +0530

    readme image update

--- client/src/Components/ChatInput.jsx
+++ client/src/Components/ChatInput.jsx
@@ -11,6 +11,7 @@ function ChatInput({ Question, setLoading, data, setdata }) {
 
     const ref = useRef(null);
     const [text, setText] = useState("");
+    const [mic, Setmic] = useState(false);
     const { transcript, resetTranscript } = useSpeechRecognition({
         continuous: true
     });
@@ -18,7 +19,9 @@ function ChatInput({ Question, setLoading, data, setdata }) {
     if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
         return "Your Broswer is not support voice search please use different broswer";
     }
+
     const handleSubmit = async () => {
+        Setmic(false);
         setLoading(true);
         var obj = {
             name: ref.current.value,
@@ -37,7 +40,7 @@ function ChatInput({ Question, setLoading, data, setdata }) {
             message: transcript
         }
         var Message = msg.message ? msg : msg2
-        setText("");  
+        setText("");
         var res = await getQuestion(Message);
         // SpeechRecognition.resetTranscript();
         ref.current.value = "";
@@ -52,7 +55,8 @@ function ChatInput({ Question, setLoading, data, setdata }) {
     // by using click
     const ClickHandler = async (e) => {
         if (e.key == "Enter") {
-            setText(""); 
+            Setmic(false);
+            setText("");
             setLoading(true);
             var obj = {
                 name: ref.current.value,
@@ -71,9 +75,9 @@ function ChatInput({ Question, setLoading, data, setdata }) {
                 message: transcript
             }
             var Message = msg.message ? msg : msg2
-            setText("");  
+            setText("");
             var res = await getQuestion(Message);
-         
+
             ref.current.value = "";
 
             var receiver = {
@@ -89,13 +93,22 @@ function ChatInput({ Question, setLoading, data, setdata }) {
         setText(e.target.value)
     }
     const handleStart = () => {
+        Setmic(true);
         setText(transcript)
+        setTimeout(() => {
+            Setmic(false);
+        }, 6000);
+
     }
 
     return (
         <Box fontFamily={'Lora'} w={["95%", "80%"]} display={'flex'} h="10vh" m="auto" boxShadow='dark-lg' mt="10px" p="15px" borderRadius={'10px'}>
-            <Img pr="5px" h="38px" _hover={{ cursor: "pointer" }} onClick={() => { SpeechRecognition.startListening(); handleStart() }} src="https://www.iconpacks.net/icons/1/free-microphone-icon-342-thumb.png" />
-            <Input h="40px" color={'blue'} type="text" ref={ref} value={text?text:transcript}  onKeyPress={ClickHandler} onKeyUp={resetTranscript} onChange={voiceHandler} placeholder="Type whatever you want" />
+            {mic ? <Img pr="5px" w="45px" borderRadius={'50%'} _hover={{ cursor: "pointer" }}
+                src="https://upload.wikimedia.org/wikipedia/commons/0/06/Mic-Animation.gif" />
+                : <Img pr="5px" h="38px" _hover={{ cursor: "pointer" }}
+                    onClick={() => { SpeechRecognition.startListening(); handleStart() }}
+                    src="https://www.iconpacks.net/icons/1/free-microphone-icon-342-thumb.png" />}
+            <Input h="40px" color={'blue'} type="text" ref={ref} value={text ? text : transcript} onKeyPress={ClickHandler} onKeyUp={resetTranscript} onChange={voiceHandler} placeholder="Type or speak on mic whatever you want" />
             <Img _hover={{ cursor: "pointer" }} onClick={() => { handleSubmit(); resetTranscript() }} pl="5px" h="38px" src="https://w7.pngwing.com/pngs/891/367/png-transparent-computer-icons-symbol-send-email-button-miscellaneous-blue-angle.png" alt="" />
         </Box>
     );
